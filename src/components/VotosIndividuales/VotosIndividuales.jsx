6f75ce446dba7ebdd192bce0fd2a65d116733939
JaquelineRocio <jaquelineramosvargas@gmail.com>
2024-09-24 23:29:15 -0500

    feat: implement votosindividuales component

--- src/components/VotosIndividuales/VotosIndividuales.jsx
+++ src/components/VotosIndividuales/VotosIndividuales.jsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import { useVoting } from '../VotingProvider/VotingProvider';
+
 function VotosIndividuales() {
-  return <main>holi</main>;
+  const { votes, filter } = useVoting();
+  const totalVotes = Object.values(votes).reduce((acc, curr) => acc + curr, 0);
+
+  return (
+    <div>
+      {Object.keys(votes).map(
+        (key) =>
+          filter.selectedCandidates.includes(key) && (
+            <div key={key}>
+              {key.charAt(0).toUpperCase() + key.slice(1)}:{' '}
+              {filter.showPercentage
+                ? `${((votes[key] / totalVotes) * 100).toFixed(2)}%`
+                : votes[key]}
+            </div>
+          ),
+      )}
+    </div>
+  );
 }
 
 export default VotosIndividuales;
